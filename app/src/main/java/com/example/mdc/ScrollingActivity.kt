dcd2d8c7ce81beb5f025a352145862c98996c3da
jonathan <jonathan.marquib>
2022-03-02 09:38:25 -0600

    Commit final de material design

--- app/src/main/java/com/example/mdc/ScrollingActivity.kt
+++ app/src/main/java/com/example/mdc/ScrollingActivity.kt
@@ -1,13 +1,18 @@
 package com.example.mdc
 
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.webkit.URLUtil
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.mdc.databinding.ActivityScrollingBinding
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class ScrollingActivity : AppCompatActivity() {
 
@@ -18,6 +23,80 @@ class ScrollingActivity : AppCompatActivity() {
         binding = ActivityScrollingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        /*findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            if (findViewById<BottomAppBar>(R.id.bottom_app_bar).fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+                findViewById<BottomAppBar>(R.id.bottom_app_bar).fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+               }  else { findViewById<BottomAppBar>(R.id.bottom_app_bar).fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            }
+        }*/
+
+        binding.fab.setOnClickListener {
+            if (binding.bottomAppBar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER){
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+               }  else { binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            }
+        }
+
+        binding.bottomAppBar.setNavigationOnClickListener({
+            Snackbar.make(binding.root,R.string.message_action_success,Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .show()
+        })
+
+        binding.content.btnSkip.setOnClickListener { binding.content.cvAd.visibility = View.GONE }
+
+        binding.content.btnBuy.setOnClickListener {
+            Snackbar.make(it, R.string.card_buying, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .setAction(R.string.card_to_go){
+                    Toast.makeText(this, R.string.card_historial, Toast.LENGTH_SHORT).show()
+                }.show()
+        }
+
+        loadImage()
+
+        binding.content.cbEnablePass.setOnClickListener {
+            binding.content.tiPassword.isEnabled = !binding.content.tiPassword.isEnabled
+        }
+
+        binding.content.etUrl.onFocusChangeListener = View.OnFocusChangeListener { view, focused ->
+            var errorStr:String? = null
+            val url = binding.content.etUrl.text.toString()
+
+            if (!focused){//con el signo ! se niega la sentencia, es decir, cuando no tenga el foco
+                when {
+                    url.isEmpty() -> {
+                        errorStr  = getString(R.string.card_required)
+                    }
+                    URLUtil.isValidUrl(url) -> {
+                        loadImage(url)
+                    }
+                    else -> {
+                        errorStr = getString(R.string.card_invalid_url)
+                    }
+                }
+            }
+            binding.content.tiUrl.error
+        }
+
+        binding.content.toogleButton.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            binding.content.root.setBackgroundColor(
+                when(checkedId){
+                    R.id.btnRed -> Color.RED
+                    R.id.btnBlue -> Color.BLUE
+                    else -> Color.GREEN
+                }
+            )
+
+        }
+    }
+
+    private fun loadImage(url:String = "https://i.ytimg.com/vi/4B79tHErbiA/hqdefault.jpg"){
+        Glide.with(this)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(binding.content.imgCover)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
